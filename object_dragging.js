dfc325aa88073182db621e6bdbb43886f68f0037
Kevin <tbnrflamekun321@gmail.com>
2022-03-20 00:43:16 -0700

    lol

--- object_dragging.js
+++ object_dragging.js
@@ -1,8 +1,9 @@
 let background_od;
 function setup() {
   createCanvas(1000, 1300);
-  background_od = loadImage('assests/background_od.png');
+  background_od = loadImage('ilia assest/assests/392x696bb.png');
 }
 function draw_odbg(){
   background(background_od);
+  
 }
