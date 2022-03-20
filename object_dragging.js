9994da030302afcb88433940c775b94c994ad770
Kevin <tbnrflamekun321@gmail.com>
2022-03-19 18:25:54 -0700

    brief

--- object_dragging.js
+++ object_dragging.js
@@ -1,6 +1,10 @@
 let background_od;
 function setup() {
   createCanvas(1000, 1300);
-  background_od = loadImage('assests\background_od.gif');
+  background_od = loadImage('assests/background_od.png');
+}
+function draw_odbg(){
+  background(background_od);
+
 }
 
