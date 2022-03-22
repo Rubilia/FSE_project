617f392111bb78c6ec55a3fecc919259a640dcb2
rubilia <rubiliam2003@gmail.com>
2022-03-22 00:51:07 -0700

    Code has been rewritten with the help of CallHandler

--- music_tiles.js
+++ music_tiles.js
@@ -1,14 +1,16 @@
 // Hyperparameters
 const canvas_width = 1000;
 const canvas_height = 1300;
-
+const tile_size_x = 200;
 
 let tile_speed = 300;
-const tile_size_x = 200;
 let active_screen = 0;
 /* 
-0 - 
+0 - main menu of the game
+1 - main menu with the settings menu
 */
+
+let CHandler;
 
 // Variables for gradient background
 const min_bg_brightness = 70;
@@ -18,102 +20,53 @@ let bg_color_change_speed_2 = [.1, -.3, .1];
 let bg_c1 = [0, 0, 100];
 let bg_c2 = [210, 100, 80];
 
+// Variables gor buttons and etc
+let canvas;
+let settings_btn;
+
+// Preloaded images
+let settings_img;
+
+function preload_images(){
+  settings_img = loadImage('assets/settings_tiles_icon.png');
+  settings_img.resize(80, 80);
+}
+
 function setup() {
-    cnv = createCanvas(canvas_width, canvas_height);
-    cnv.mouseClicked(addTile);
-    // c1 = color(bg_c1[0], bg_c1[1], bg_c1[2]);
-    // c2 = color(63, 191, 191);
-    
-    // for(let y=0; y<height; y++){
-    //   n = map(y,0,height,0,1);
-    //   let newc = lerpColor(c1, c2, n);
-    //   stroke(newc);
-    //   line(0,y,width, y);
-    // }
-    draw_main_menu();
+    // Preload images
+    preload_images();
+
+    // Initialize variables
+    CHandler = new CallHandler();
+
+    canvas = createCanvas(canvas_width, canvas_height);
+    canvas.mouseClicked(addTile);
+
+    // Add tasks to draw basic UI
+    CHandler.add_callable("draw_gradient_background", draw_gradient_background, -1, {})
+    CHandler.add_callable("draw_main_menu", draw_main_menu, -1, {"settings_angle": 0, "angle_rate": PI / 180})
   }
   
   function draw() {
-    // return;
-    if (active_screen == 0){
-      // Draw game menu
-      draw_gradient_background();
-      draw_main_menu();
-      
-      return;
-    }
+    // Execute all pending callbacks
+      CHandler.execute();
+  }
+
+
+  function draw_main_menu(args){
+    angle = args["settings_angle"] + args["angle_rate"];
+    rotate(angle);
+    settings_btn = image(settings_img, canvas_width - 100, canvas_height - 100, 80, 80);
+    // settings_btn.mousePressed(() => {active_screen = 1;});
+    rotate(0);
     
-
-    dt = deltaTime / 1000;
-    for(i = 0 ; i < tiles.length ;i++){
-      tiles[i].move(dt)
-    }
-  }
-
-  function draw_gradient_background(){
-    // Switch to HSB
-    colorMode(HSB, 360, 100, 100);
-
-    let c1 = color(bg_c1[0], bg_c1[1], bg_c1[2]);
-    let c2 = color(bg_c2[0], bg_c2[1], bg_c2[2]);
-
-    for(let y=0; y<canvas_height; y++){
-      n = map(y,0,canvas_height,0,1);
-      let newc = lerpColor(c1,c2,n);
-      stroke(newc);
-      line(0,y,canvas_width, y);
-    }
-
-    // Change colors
-    bg_c1[0] = (bg_c1[0] + (bg_color_change_speed_1[0] * random(0.8, 1.2)));
-    if (bg_c1[0] <= 0 || bg_c1[0] >= 360){
-      bg_color_change_speed_1[0] *= -1;
-      bg_c1[0] = max(0, min(bg_c1[0], 360));
-    }
-      
-    bg_c2[0] = (bg_c2[0] + (bg_color_change_speed_2[0] * random(0.8, 1.2)));
-    if (bg_c2[0] <= 0 || bg_c2[0] >= 360){
-      bg_color_change_speed_2[0] *= -1;
-      bg_c2[0] = max(0, min(bg_c2[0], 360));
-    }
-
-    bg_c1[1] = (bg_c1[1] + (bg_color_change_speed_1[1] * random(0.8, 1.2)));
-    if (bg_c1[1] <= 0 || bg_c1[1] >= 100){
-      bg_color_change_speed_1[1] *= -1;
-      bg_c1[1] = max(0, min(bg_c1[1], 100));
-    }
-
-    bg_c2[1] = (bg_c2[1] + (bg_color_change_speed_2[1] * random(0.8, 1.2)));
-    if (bg_c2[1] <= 0 || bg_c2[1] >= 100){
-      bg_color_change_speed_2[1] *= -1;
-      bg_c2[1] = max(0, min(bg_c2[1], 100));
-    }
-
-    bg_c1[2] = (bg_c1[2] + (bg_color_change_speed_1[2] * random(0.8, 1.2)));
-    if (bg_c1[2] <= min_bg_brightness || bg_c1[2] >= 100){
-      bg_color_change_speed_1[2] *= -1;
-      bg_c1[2] = max(min_bg_brightness, min(bg_c1[2], 100));
-    }
-
-    bg_c2[2] = max((bg_c2[2] + (bg_color_change_speed_2[2] * random(0.8, 1.2))));
-    if (bg_c2[2] <= min_bg_brightness || bg_c2[2] >= 100){
-      bg_color_change_speed_2[2] *= -1;
-      bg_c2[2] = max(min_bg_brightness, min(bg_c2[2], 100));
-    }
-
-    // Switch back to RGB
-    colorMode(RGB, 255, 255, 255, 255);
-  }
-
-  function draw_main_menu(){
-    settings = createImg('assets/settings_tiles_icon.png');
-    settings.position(canvas_width - 100, canvas_height - 100);
-    settings.size(80, 80);
-    settings.mousePressed(draw_settings_menu);
+    args["settings_angle"] = angle;
+    return args;
   }
 
   function draw_settings_menu(){
-
+    fill(color(0, 0, 0, 255));
+    rect(0, 0, canvas_width, canvas_height);
   }
 
   function addTile(){
@@ -127,28 +80,113 @@ class CallHandler{
     this.internal_states = [];
   }
 
-  add_callable(callable, repetitions, args={}){
-    append(this.callables, [callable, repetitions]);
+  add_callable(name, callable, repetitions, args={}){
+    args["call_handler"] = this;
+    args["name"] = name;
+
+    append(this.callables, [callable, repetitions, name]);
     append(this.internal_states, args);
   }
 
+  // Executes callables from list, updates internal states and number of reps left
   execute(){
-    new_callables = [];
-    new_states = [];
+    // draw_gradient_background(0);
+    let new_callables = [];
+    let new_states = [];
     for (let i = 0; i < this.callables.length; i++){
-        call = this.callable[i][0];
-        reps = this.callables[i][1] - 1;
-        previous_state = this.internal_states[i];
+        let call = this.callables[i][0];
+        let reps = this.callables[i][1];
+        reps -= (reps == -1? 0: 1);
+
+        let previous_state = this.internal_states[i];
         
-        state = call(previous_state);
-        if (reps > 0){
-          append(new_callables, [call, state]);
+        let state = call(previous_state);
+        if (reps > 0 || reps == -1){
+          append(new_callables, [call, reps, this.callables[i][2]]);
           append(new_states, state);
         }
     }
     this.callables = new_callables;
     this.internal_states = new_states;
   }
+
+  // Function to remove callables by name
+  remove_callback(name){
+    new_callables = [];
+    new_internal_states = [];
+    for (let i = 0; i < this.callables.length; i++){
+      // Remove callables with a given name
+      if (name == this.callables[i][2])
+        continue;
+
+      // If a callable has a different name - ignore it
+      append(new_callables, this.callables[i]);
+      append(new_internal_states, this.internal_states[i]);
+    }
+  }
+}
+
+function draw_gradient_background(args){
+  // Check whether the mode is correct
+  if (!(active_screen == 0 || active_screen == 1))
+    return;
+
+  // Switch to HSB
+  colorMode(HSB, 360, 100, 100);
+
+  let c1 = color(bg_c1[0], bg_c1[1], bg_c1[2]);
+  let c2 = color(bg_c2[0], bg_c2[1], bg_c2[2]);
+
+  for(let y=0; y<canvas_height; y++){
+    n = map(y,0,canvas_height,0,1);
+    let newc = lerpColor(c1,c2,n);
+    stroke(newc);
+    line(0,y,canvas_width, y);
+  }
+
+  noStroke();
+
+  // Change colors
+  bg_c1[0] = (bg_c1[0] + (bg_color_change_speed_1[0] * random(0.8, 1.2)));
+  if (bg_c1[0] <= 0 || bg_c1[0] >= 360){
+    bg_color_change_speed_1[0] *= -1;
+    bg_c1[0] = max(0, min(bg_c1[0], 360));
+  }
+    
+  bg_c2[0] = (bg_c2[0] + (bg_color_change_speed_2[0] * random(0.8, 1.2)));
+  if (bg_c2[0] <= 0 || bg_c2[0] >= 360){
+    bg_color_change_speed_2[0] *= -1;
+    bg_c2[0] = max(0, min(bg_c2[0], 360));
+  }
+
+  bg_c1[1] = (bg_c1[1] + (bg_color_change_speed_1[1] * random(0.8, 1.2)));
+  if (bg_c1[1] <= 0 || bg_c1[1] >= 100){
+    bg_color_change_speed_1[1] *= -1;
+    bg_c1[1] = max(0, min(bg_c1[1], 100));
+  }
+
+  bg_c2[1] = (bg_c2[1] + (bg_color_change_speed_2[1] * random(0.8, 1.2)));
+  if (bg_c2[1] <= 0 || bg_c2[1] >= 100){
+    bg_color_change_speed_2[1] *= -1;
+    bg_c2[1] = max(0, min(bg_c2[1], 100));
+  }
+
+  bg_c1[2] = (bg_c1[2] + (bg_color_change_speed_1[2] * random(0.8, 1.2)));
+  if (bg_c1[2] <= min_bg_brightness || bg_c1[2] >= 100){
+    bg_color_change_speed_1[2] *= -1;
+    bg_c1[2] = max(min_bg_brightness, min(bg_c1[2], 100));
+  }
+
+  bg_c2[2] = max((bg_c2[2] + (bg_color_change_speed_2[2] * random(0.8, 1.2))));
+  if (bg_c2[2] <= min_bg_brightness || bg_c2[2] >= 100){
+    bg_color_change_speed_2[2] *= -1;
+    bg_c2[2] = max(min_bg_brightness, min(bg_c2[2], 100));
+  }
+
+  // Switch back to RGB
+  colorMode(RGB, 255, 255, 255, 255);
+
+  return args;
 }
 
 class TileManager{
