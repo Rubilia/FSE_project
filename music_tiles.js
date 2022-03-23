f261f8d38a2a6247e5464e67fe9ac2f310a28217
rubilia <rubiliam2003@gmail.com>
2022-03-23 13:31:58 -0700

    andom movement of tiles in the bg

--- music_tiles.js
+++ music_tiles.js
@@ -3,17 +3,19 @@ const canvas_width = 1000;
 const canvas_height = 1300;
 const tile_size_x = 200;
 
-let tile_speed = 300;
+let tile_speed = 400;
 let active_screen = 0;
 /* 
 0 - main menu of the game
 1 - main menu + animation of settings menu (open animation)
+2 - main_menu + settings menu on top of it
 */
 
 let CHandler;
 
 // Variables for gradient background
 const min_bg_brightness = 70;
+const max_bg_saturation = 80;
 let bg_color_change_speed_1 = [-.3, .1, -.1];
 let bg_color_change_speed_2 = [.1, -.3, .1];
 
@@ -32,6 +34,16 @@ function preload_images(){
   settings_img.resize(80, 80);
 }
 
+function draw() {
+  // Execute all pending callbacks
+    CHandler.execute();
+}
+
+// Handles all mouse clicks through CallHandler
+function mouseClicked() {
+    CHandler.execute_click();
+}
+
 function setup() {
     // Preload images
     preload_images();
@@ -44,50 +56,88 @@ function setup() {
 
     // Add tasks to draw basic UI
     CHandler.add_callable("draw_gradient_background", draw_gradient_background, -1, {});
-    CHandler.add_callable("draw_main_menu", draw_main_menu, -1, {"settings_angle": 0, "angle_rate": PI / 180});
+    CHandler.add_callable("random_tiles_animation", draw_random_tiles, -1, {"tiles_manager": new TileManager(), "idle": 2})
+    CHandler.add_callable("draw_main_menu", draw_main_menu, -1, {"settings_angle": 0, "angle_rate": PI / 30, "do_rotation": false});
   }
   
-  function draw() {
-    // Execute all pending callbacks
-      CHandler.execute();
-  }
+  function draw_random_tiles(args){
+    args["idle"] -= 1;
+    manager = args["tiles_manager"];
+    
+    if (args["idle"] == 0){
+        try{
+          corridor = random([0, 1, 2, 3]);
+          tile_size = random([new TileSizes(tile_size_x, 200), new TileSizes(tile_size_x, 400), new TileSizes(tile_size_x, 600), new TileSizes(tile_size_x, 800)]);
 
-  // Handles all mouse clicks through CallHandler
-  function mouseClicked() {
-      CHandler.execute_click();
-  }
+          manager.add_tile(corridor, tile_size);
+          args["idle"] = int(random(40, 100));
+        } catch(ex){
+          console.log(ex);
+        }
+    }
 
+    manager.move(deltaTime);
+    return args;
+  }
 
   function draw_main_menu(args){
-    angle = args["settings_angle"] + args["angle_rate"];
+    // Draw settings button!
 
     // Rotation
     translate(canvas_width - 60, canvas_height - 60);
     imageMode(CENTER);
-    rotate(angle);
 
-    let f1 = ((args) => {
-      if (!(active_screen == 0 || active_screen == 1)) return false;
-      d = (dist(mouseX, mouseY, canvas_width - 60, canvas_height - 60));
-      return (dist(mouseX, mouseY, canvas_width - 100, canvas_height - 100)) <= 70;
+    
+    if (args["do_rotation"]){
+        angle = args["settings_angle"] + args["angle_rate"];
+        rotate(angle);
+    }
+
+    let identify_region_callback = ((args) => {
+        if (!(active_screen == 0 || active_screen == 1)) return false;
+        d = (dist(mouseX, mouseY, canvas_width - 60, canvas_height - 60));
+        return (dist(mouseX, mouseY, canvas_width - 100, canvas_height - 100)) <= 70;
     });
 
-    let f2 = ((args) => {
-      // Function called when settings button is clicked
-      active_screen = ;
-      return args;
+    let button_pressed_callbak = ((args) => {
+        if (CHandler.does_callback_exist("delayed_disable_settings_rotation")){
+            return args;
+        }
+        // Function called when settings button is clicked
+        active_screen = 1;
+
+        // Allow rotation of settings button
+        args_ = CHandler.get_internal_state("draw_main_menu");
+        args_["do_rotation"] = true;
+        args_["settings_angle"] = 0;
+        CHandler.update_internal_state("draw_main_menu", args_);
+
+        // Create a task to disable rotation after 360 rotation
+        CHandler.add_callable("delayed_disable_settings_rotation", ((args) => {
+            args["counter"] -= 1;
+            if (args["counter"] == 0){
+                args_ = CHandler.get_internal_state("draw_main_menu");
+                args_["do_rotation"] = false;
+                CHandler.update_internal_state("draw_main_menu", args_);
+            }
+            return args;
+        }), 60, {"counter": 60});
+
+        return args;
     });
 
     settings_btn = image(settings_img, 0, 0, 80, 80);
-    CHandler.add_clickable_region("settings_btn_click", f1, f2, {});
+    CHandler.add_clickable_region("settings_btn_click", identify_region_callback, button_pressed_callbak, {});
 
-
-    // settings_btn.mousePressed(() => {active_screen = 1;});
-    
-    rotate(-angle);
+    if (args["do_rotation"]){
+        rotate(-angle);
+        args["settings_angle"] = angle;
+    }
     translate(60 - canvas_width, 60 - canvas_height);
+
+    // Draw buttons
+
     
-    args["settings_angle"] = angle;
     return args;
   }
 
@@ -98,6 +148,40 @@ class CallHandler{
 
     this.click_callables = [];
     this.click_internal_states = [];
+  }
+
+  reset_callbacks(){
+    this.callables = [];
+    this.internal_states = [];
+
+    this.click_callables = [];
+    this.click_internal_states = [];
+  }
+
+  does_callback_exist(name){
+    for (let i = 0; i < this.callables.length; i++){
+      // Remove callables with a given name
+      if (name == this.callables[i][2])
+        return true;
+    }
+    return false;
+  }
+
+  get_internal_state(name){
+    for (let i = 0; i < this.callables.length; i++){
+      // Remove callables with a given name
+      if (name == this.callables[i][2])
+        return this.internal_states[i];
+    }
+    return {};
+  }
+
+  update_internal_state(name, args){
+    for (let i = 0; i < this.callables.length; i++){
+      // Remove callables with a given name
+      if (name == this.callables[i][2])
+        this.internal_states[i] = args;
+    }
   }
 
   add_clickable_region(name, is_inside_callable, click_callable, args){
@@ -189,7 +273,7 @@ class CallHandler{
 
 function draw_gradient_background(args){
   // Check whether the mode is correct
-  if (!(active_screen == 0 || active_screen == 1))
+  if (!(active_screen == 0 || active_screen == 1 || active_screen == 2))
     return;
 
   // Switch to HSB
@@ -221,15 +305,15 @@ function draw_gradient_background(args){
   }
 
   bg_c1[1] = (bg_c1[1] + (bg_color_change_speed_1[1] * random(0.8, 1.2)));
-  if (bg_c1[1] <= 0 || bg_c1[1] >= 100){
+  if (bg_c1[1] <= 0 || bg_c1[1] >= max_bg_saturation){
     bg_color_change_speed_1[1] *= -1;
-    bg_c1[1] = max(0, min(bg_c1[1], 100));
+    bg_c1[1] = max(0, min(bg_c1[1], max_bg_saturation));
   }
 
   bg_c2[1] = (bg_c2[1] + (bg_color_change_speed_2[1] * random(0.8, 1.2)));
-  if (bg_c2[1] <= 0 || bg_c2[1] >= 100){
+  if (bg_c2[1] <= 0 || bg_c2[1] >= max_bg_saturation){
     bg_color_change_speed_2[1] *= -1;
-    bg_c2[1] = max(0, min(bg_c2[1], 100));
+    bg_c2[1] = max(0, min(bg_c2[1], max_bg_saturation));
   }
 
   bg_c1[2] = (bg_c1[2] + (bg_color_change_speed_1[2] * random(0.8, 1.2)));
@@ -256,26 +340,38 @@ class TileManager{
   }
 
   add_tile(corridor, size){
-    append(tiles, new Tile(size.size_x, size.size_y, tile_speed, corridor));
+    try{
+        append(this.tiles, new Tile(size.size_x, size.size_y, tile_speed, corridor));
+    } catch(ex){
+      console.log(ex);
+    }
+    
   }
 
   move(dt){
-      tmp_tiles = []
-      for(i = 0 ; i < tiles.length ;i++){
-        tiles[i].move(dt)
-        if (!this.tiles[i].is_out())
+      let tmp_tiles = []
+      for(let i = 0 ; i < this.tiles.length ;i++){
+        this.tiles[i].move(dt)
+        if (!this.tiles[i].is_out()){
           append(tmp_tiles, this.tiles[i]);
-        else
-          remove(this.tiles[i]);
+        }
       }
       this.tiles = tmp_tiles;
-
+      
+      // Draw borders
+      fill("grey");
+      noStroke();
+      rect(98, 0, 2, canvas_height);
+      rect(299, 0, 2, canvas_height);
+      rect(499, 0, 2, canvas_height);
+      rect(701, 0, 2, canvas_height);
+      rect(902, 0, 2, canvas_height);
   }
 }
 
 class Tile {
   constructor(size_x=tile_size_x, size_y, speed_y=tile_speed, corridor){
-    this.x = 100 + corridor * 200;
+    this.x = 100 + corridor * 200 + (corridor - 2);
     this.y = -size_y;
     this.v_y = speed_y;
     this.size_x = size_x;
@@ -287,7 +383,7 @@ class Tile {
   }
 
   move(dt){
-    this.y = this.y + this.v_y * dt;
+    this.y = this.y + this.v_y * dt / 1000.;
     this.draw();
   }
 
@@ -303,10 +399,14 @@ class TileSizes {
     this.size_y = size_y;
   }
 
-  static small = new TileSizes(tile_size_x, 100)
-  static medium = new TileSizes(tile_size_x, 400)
-  static large = new TileSizes(tile_size_x, 600)
-  static enormous = new TileSizes(tile_size_x, 800)
+  clone(){
+    return new TileSizes(this.size_x, this.size_y);
+  }
+
+  static small = new TileSizes(tile_size_x, 100);
+  static medium = new TileSizes(tile_size_x, 400);
+  static large = new TileSizes(tile_size_x, 600);
+  static enormous = new TileSizes(tile_size_x, 800);
 
 }
 
